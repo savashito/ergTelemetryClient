74473f415f9fd2a45cdc773e92d3d491608134bd
Rodrigo Savage <rodrigosavage@gmail.com>
2016-06-06 10:04:16 -0700

    Added socket connection

--- main.py
+++ main.py
@@ -1,13 +1,27 @@
 import sys
+from socketIO_client import SocketIO, LoggingNamespace
 sys.path.append("../PyRow")
 import pyrow
 
+addr = '192.168.0.3'
 ergs = pyrow.find()
-number_ergs = len(list(dev))
-print number_ergs
-erg = ergs.next()
-m = pyrow.pyrow(erg)
-print m.get_status()
-while 1:
-	data = m.get_monitor()
-	print data
+number_ergs = len(list(ergs))
+print "Ergs connected:" + str(number_ergs)
+if(number_ergs>0):
+	erg = ergs.next()
+	m = pyrow.pyrow(erg)
+	print m.get_status()
+	with SocketIO(addr, 80, LoggingNamespace) as socketIO:
+		while 1:
+			data = m.get_monitor()
+			print data
+			socketIO.emit('ergData',data)
+
+
+
+
+with SocketIO(addr, 80, LoggingNamespace) as socketIO:
+    # socketIO.emit('connection')
+    # socketIO.wait(seconds=1)
+    for i in range(10):
+    	socketIO.emit('ergData',{'Muerte': 'world '+str(i)})
