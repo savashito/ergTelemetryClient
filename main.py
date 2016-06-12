c612edf7bc8fa0fe7a0c51764702d8dc29253b14
Rodrigo Savage <rodrigosavage@gmail.com>
2016-06-11 18:40:30 -0700

    Change ip to aws server

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
 import sys
+import time
 from socketIO_client import SocketIO, LoggingNamespace
 sys.path.append("../PyRow")
 import pyrow
 
-addr = '192.168.0.3'
+addr = '54.153.114.110'# '192.168.0.3'
 ergs = pyrow.find()
 ergs = list(ergs)
 number_ergs = len(ergs)
@@ -12,9 +13,12 @@ if(number_ergs>0):
 	erg = ergs[0]
 	m = pyrow.pyrow(erg)
 	print m.get_status()
+	ergInfo = m.get_erg()
+	# cid serial
 	with SocketIO(addr, 80, LoggingNamespace) as socketIO:
 		while 1:
-			data = m.get_monitor()
+			data = m.get_monitor(forceplot=True)
+			data['cid'] = ergInfo['cid']
 			print data
 			socketIO.emit('ergData',data)
 
@@ -23,8 +27,21 @@ if(number_ergs>0):
 
 with SocketIO(addr, 80, LoggingNamespace) as socketIO:
     # socketIO.emit('connection')
+    data = {'cid':2,'status': 9, 'distance': 11.2, 'heartrate': 0, 'power': 6, 'calhr': 320.6496, 'calories': 0, 'pace': 387.8277952417603, 'spm': 51, 'time': 9.29}
     # socketIO.wait(seconds=1)
-    for i in range(10):
-    	socketIO.emit('ergData',{'Muerte': 'world '+str(i)})
+    # for i in range(10):
+    totalTime = 0
+    deltaTime = 0.5 # s
+    deltaDistance = 5 # m per s
+    totalDistance = 0
+    while 1:
+    	data['distance'] = totalDistance
+    	data['time'] = totalTime
+    	socketIO.emit('ergData',data)
+    	time.sleep(deltaTime)
+    	totalDistance = totalDistance +deltaDistance*deltaTime
+    	totalTime = totalTime+deltaTime
+
+    	# socketIO.emit('boop',{'data','meow','Muerte': 'world '+str(i)})
 
     socketIO.wait(seconds=1)
