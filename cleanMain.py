89d535b0ecbfbc72e5bd9d2b746904530db72e91
Rodrigo Savage <rodrigosavage@Rodrigos-MacBook-Air.local>
2017-06-02 17:35:34 -0400

    threading

--- cleanMain.py
+++ cleanMain.py
@@ -32,72 +32,95 @@ def searchErgs():
 # m = pyrow.pyrow(erg)
 # # print m.get_status()
 # ergInfo = m.get_erg()
-num_zeros = [0,0,0,0]
-num_ones = [0,0,0,0]
-prev_time = [0,0,0,0]
-m = [0,0,0,0]
-ergs = searchErgs()
-n_ergs = 0
-
-while(1):
-	ergs = pyrow.find()
-	ergs = list(ergs)
-	if n_ergs != len(ergs):
-		for i in range(n_ergs,len(ergs)):
-			print i
-			time.sleep(1)
-
-			ergs = pyrow.find()
-			ergs = list(ergs)
-			m[i] = pyrow.pyrow(ergs[i])	
 
 
-		n_ergs=	len(ergs)
-	try:
-		for i in range(n_ergs):
-			# m = pyrow.pyrow(ergs[i])
-			# print m.get_status()
-			ergInfo = m[i].get_erg()
-			data = m[i].get_monitor(forceplot=True)
-			data['cid'] = ergInfo['cid']
-			data['i'] = i
-		#	print data
-			#print data['forceplot']
-		
-			if(0==len(data['forceplot'])):
-				num_zeros[i] +=1
-				if(num_ones[i]>3):
-					print "endedstarted "+str(prev_time[i])+" " +str(data['time'])
-					data['driveTime']=data['time']-prev_time[i] 
-					prev_time[i] = data['time']
-					socketIO.emit('strokeData',data)
-				num_ones[i]=0
 
-			else:
-				num_ones[i] +=1
-				#print num_zeros
-				if(num_zeros[i]>3):
-					
-					print "rowe started "+str(prev_time[i])+" " +str(data['time'])
-					data['strokeRecoveryTime']=data['time']-prev_time[i] 
-					prev_time[i] = data['time']
-					socketIO.emit('strokeData',data)
-				num_zeros[i] = 0
-#			print data
-			socketIO.emit('ergData',data)
+import threading
+
+
+def USBErgService (  ):
+	running = True;
+	num_zeros = [0,0,0,0]
+	num_ones = [0,0,0,0]
+	prev_time = [0,0,0,0]
+	m = [0,0,0,0]
+	ergs = searchErgs()
+	n_ergs = 0
+	while(running):
 		ergs = pyrow.find()
-		# print len(list(ergys))
-		# ergs = list(ergs)
-		# number_ergs = len(ergs)
-			# time.sleep(0.01)
-			# ergs = searchErgs()
-	
-	except IOError as e:
-		n_ergs = 0
-		time.sleep(1)
-		print e
-		print "Search for ergs again"
-		erg = searchErgs()[0]
-		m = pyrow.pyrow(erg)
-		# print m.get_status()
-		ergInfo = m.get_erg()
+		ergs = list(ergs)
+		if n_ergs != len(ergs):
+			# new erg connection
+			running = False
+			return
+			# for i in range(n_ergs,len(ergs)):
+				# running = False
+				# break
+				# print i
+				# time.sleep(1)
+
+				# ergs = pyrow.find()
+				# ergs = list(ergs)
+				# m[i] = pyrow.pyrow(ergs[i])	
+			# n_ergs=	len(ergs)
+		try:
+			for i in range(n_ergs):
+				# m = pyrow.pyrow(ergs[i])
+				# print m.get_status()
+				ergInfo = m[i].get_erg()
+				data = m[i].get_monitor(forceplot=True)
+				data['cid'] = ergInfo['cid']
+				data['i'] = i
+			#	print data
+				#print data['forceplot']
+			
+				if(0==len(data['forceplot'])):
+					num_zeros[i] +=1
+					if(num_ones[i]>3):
+						print "endedstarted "+str(prev_time[i])+" " +str(data['time'])
+						data['driveTime']=data['time']-prev_time[i] 
+						prev_time[i] = data['time']
+						socketIO.emit('strokeData',data)
+					num_ones[i]=0
+
+				else:
+					num_ones[i] +=1
+					#print num_zeros
+					if(num_zeros[i]>3):
+						
+						print "rowe started "+str(prev_time[i])+" " +str(data['time'])
+						data['strokeRecoveryTime']=data['time']-prev_time[i] 
+						prev_time[i] = data['time']
+						socketIO.emit('strokeData',data)
+					num_zeros[i] = 0
+	#			print data
+				socketIO.emit('ergData',data)
+			ergs = pyrow.find()
+			# print len(list(ergys))
+			# ergs = list(ergs)
+			# number_ergs = len(ergs)
+				# time.sleep(0.01)
+				# ergs = searchErgs()
+		
+		except IOError as e:
+			running=False;
+			n_ergs = 0
+			# time.sleep(1)
+			print e
+			print "Search for ergs again"
+			return
+
+			# erg = searchErgs()[0]
+			# m = pyrow.pyrow(erg)
+			# print m.get_status()
+			# ergInfo = m.get_erg()
+USBErgServiceT = threading.Thread(name='daemon', target=USBErgService)
+# while(1):
+USBErgServiceT.start()
+USBErgServiceT.join()
+USBErgServiceT.start()
+USBErgServiceT.join()
+USBErgServiceT.start()
+USBErgServiceT.join()
+
+
